08a17b1713bc75ca8e652c9be6bb671940f72a8d
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-09 23:18:39 -0400

    EndPointCase3TestedPassed

--- test/test.js
+++ test/test.js
@@ -3,8 +3,19 @@ const chai = require("chai");
 const expect = chai.expect;
 
 var assert = require('assert');
+
 describe("Range (2,10)  Endpoints ShouldBe{3,9}", function(){
     it("Should return: {3,9}", function(){
         expect(methods.EndPoints("(2,10)")).to.be.eqls([3,9])
+    });
+});
+describe("Range [7,11)  Endpoints ShouldBe{7,10}", function(){
+    it("Should return: {3,9}", function(){
+        expect(methods.EndPoints("[7,11)")).to.be.eqls([7,10])
+    });
+});
+describe("Range (2,6)  Endpoints ShouldBe{3,5}", function(){
+    it("Should return: {3,5}", function(){
+        expect(methods.EndPoints("(2,6)")).to.be.eqls([3,5])
     });
 });
