82b58efa3e1d8b32d2c784d931dec78704c0a571
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-09 23:32:12 -0400

    AreEqualsTestCase7Passed

--- test/test.js
+++ test/test.js
@@ -25,13 +25,18 @@ describe("Range (7,11)  Endpoints ShouldBe{8,10}", function(){
     });
 });
 describe("Range(3, 8] areEqual[4, 9)", function(){
-    it("Should return: {8,10}", function(){
+    it("Should return: true", function(){
         expect(methods.AreEquals("(3, 8]","[4, 9)")).to.be.eqls(true)
     });
 });
 describe("Range[5, 6) areEqual(4, 5]", function () {
-    it("Should return: {8,10}", function () {
+    it("Should return: true", function () {
         expect(methods.AreEquals("[5, 6)", "(4, 5]")).to.be.eqls(true)
+    });
+});
+describe("Range(4,8) areEqual[5,7]", function () {
+    it("Should return: true", function () {
+        expect(methods.AreEquals("(4,8)", "[5,7]")).to.be.eqls(true)
     });
 });
 
