4663c1cc64a777c03507e0450b40b6e6b6aa5141
Lunaticfrg0 <emilio3scano@gmail.com>
2020-12-09 23:29:41 -0400

    AreEqualsCase6TestedPassed

--- test/test.js
+++ test/test.js
@@ -29,3 +29,9 @@ describe("Range(3, 8] areEqual[4, 9)", function(){
         expect(methods.AreEquals("(3, 8]","[4, 9)")).to.be.eqls(true)
     });
 });
+describe("Range[5, 6) areEqual(4, 5]", function () {
+    it("Should return: {8,10}", function () {
+        expect(methods.AreEquals("[5, 6)", "(4, 5]")).to.be.eqls(true)
+    });
+});
+
